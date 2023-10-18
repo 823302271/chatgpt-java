cb948667c51441abbb2a256fdc7081385a7c8a3b
zkp <823302271@qq.com>
2023-10-18 17:13:19 +0800

    修改host

--- src/main/java/com/unfbx/chatgpt/constant/OpenAIConst.java
+++ src/main/java/com/unfbx/chatgpt/constant/OpenAIConst.java
@@ -9,4 +9,6 @@ package com.unfbx.chatgpt.constant;
 public class OpenAIConst {
 
     public final static String OPENAI_HOST = "https://api.openai.com/";
+
+    public final static String fake_gpt_host = "ai.fakegpt.org";
 }
